e16a580d29b515512fd0ea351a437eda8250ec8f
Steven Schulte <spschul@umich.edu>
2017-08-25 11:39:33 -0400

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use std::env;
 use std::fs::File;
 use std::io::BufRead;
 use std::collections::HashMap;
-use std::cmp::Ordering;
 use std::f64;
 
 fn main() {
+    // Read the files
     let players_file: String = env::args().nth(1).expect(
         "Requires arguments: path to players file, path to matches file");
 
@@ -16,58 +16,47 @@ fn main() {
     let players_file = File::open(players_file).expect("Player file not found.");
     let matches_file = File::open(matches_file).expect("Matches file not found.");
 
-    let mut players_file = io::BufReader::new(&players_file);
-    let mut matches_file = io::BufReader::new(&matches_file);
+    let players_file = io::BufReader::new(&players_file);
+    let matches_file = io::BufReader::new(&matches_file);
 
-    let mut players: HashMap<String, i32> = players_file.lines().map(|line| (line.unwrap().trim().to_string(), 100)).collect();
+    // Create Players hash map
+    let mut players: HashMap<String, i32> = players_file.lines().map(|line| (line.unwrap().trim().to_string(), 1600)).collect();
 
-    println!("{:?}", players);
-
+    // Go through the matches in the file, and calculate the skill of each player
     for line in matches_file.lines() {
+        // Parse the line
         let line = line.unwrap();
         let line_segmented: Vec<&str> = line.split(',').map(|x| x.trim()).collect();
-        let player1_skill = *players.get(line_segmented[0]).unwrap();
-        let player2_skill = *players.get(line_segmented[1]).unwrap();
-        let player1_score: i32 = line_segmented[2].parse().unwrap();
-        let player2_score: i32 = line_segmented[3].parse().unwrap();
+        let skills = (*players.get(line_segmented[0]).unwrap(), *players.get(line_segmented[1]).unwrap());
+        let scores: (i32, i32) = (line_segmented[2].parse().unwrap(), line_segmented[3].parse().unwrap());
         let timestamp: u64 = line_segmented[4].parse().unwrap();
 
-        let (player1_new_skill, player2_new_skill) = 
-            calc_updated_skill(player1_skill, player2_skill, player1_score,
-                               player2_score, timestamp);
+        // calculate updated skills
+        let new_skills = calc_updated_skill(skills, scores, timestamp);
 
-        {
-            let old_skill = players.get_mut(line_segmented[0]).unwrap();
-            *old_skill = player1_new_skill;
-        }
-        {
-            let old_skill = players.get_mut(line_segmented[1]).unwrap();
-            *old_skill = player2_new_skill;
-        }
+        // update skills
+        *players.get_mut(line_segmented[0]).unwrap() = new_skills.0;
+        *players.get_mut(line_segmented[1]).unwrap() = new_skills.1;
     }
 
+    // print out each player and their skill
     for (player, skill) in &players {
         println!("{}: {}", player, skill);
     }
-
 }
 
-fn calc_updated_skill(player1_skill: i32, player2_skill: i32, player1_score: i32, player2_score: i32, timestamp: u64) -> (i32, i32) {
-    let skills = (player1_skill as f64, player2_skill as f64);
+// update skill using the Elo rating system
+// reference: https://metinmediamath.wordpress.com/2013/11/27/how-to-calculate-the-elo-rating-including-example/ 
+fn calc_updated_skill(skills: (i32, i32), scores: (i32, i32), timestamp: u64) -> (i32, i32) {
+    let skills = (skills.0 as f64, skills.1 as f64);
     let rating = (10.0f64.powf(skills.0 / 400.0f64), 10.0f64.powf(skills.1 / 400.0f64));
-    let total = skills.0 + skills.1;
-    let expected_score = (skills.0 / total, skills.1 / total);
-    let actual = if player1_score > player2_score {
+    let total = rating.0 + rating.1;
+    let expected_score = (rating.0 / total, rating.1 / total);
+    let actual = if scores.0 > scores.1 {
         ( 1.0f64, 0.0f64 )
     } else {
         ( 0.0f64, 1.0f64 )
     };
-    let K = 32.0;
-    ((skills.0 + K * (actual.0 - expected_score.0)).round() as i32, (skills.1 + K * (actual.1 - expected_score.1)).round() as i32)
-}
-
-#[derive(Debug)]
-struct Player {
-    name: String,
-    elo: i32,
+    let k = 32.0;
+    ((skills.0 + k * (actual.0 - expected_score.0)).round() as i32, (skills.1 + k * (actual.1 - expected_score.1)).round() as i32)
 }
